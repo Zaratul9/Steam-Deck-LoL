84c5815afd632791cc5a031f76fb182b5744d14c
Zaratul9 <64791818+Zaratul9@users.noreply.github.com>
2022-08-28 19:01:12 -0600

    Add files via upload

---
+++ LoL.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+WINEPREFIX=/home/deck/.var/app/com.usebottles.bottles/data/bottles/bottles/LoL /home/deck/wine/lutris-ge-lol-7.0-5-x86_64/bin/wine /home/deck/.var/app/com.usebottles.bottles/data/bottles/bottles/LoL/drive_c/Riot\ Games/Riot\ Client/RiotClientServices.exe --launch-product=league_of_legends --launch-patchline=live
